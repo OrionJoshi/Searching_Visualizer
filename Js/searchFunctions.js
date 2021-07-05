329bf41704d2a948ad6cb8c75960f1ccf3c33976
Khageshwor <khageshwarjoshi03@gmail.com>
2021-07-05 16:27:06 +0545

    Arrow moving functionality added

--- Js/searchFunctions.js
+++ Js/searchFunctions.js
@@ -2,18 +2,30 @@
 function linearSearch(arr, value) {
   let counter = 0;
   const timer = setInterval(() => {
-    let box = `box${counter}`;
+    let box = `box-wrapper-${counter}`;
+
+    if (counter != 0) {
+      // hiding arrow
+      arrowIcons[counter - 1].style.display = "none";
+    }
 
     if (counter == 10) {
       alert("Element Not Found");
       location.reload();
       clearInterval(timer);
     } else {
-      document.getElementById(box).style.backgroundColor = "red";
+      // displaying arrow
+      arrowIcons[counter].style.display = "block";
+      var innerTimer = setTimeout(() => {
+        document.getElementById(box).style.backgroundColor = failureColor;
+      }, 500);
     }
 
     if (arr[counter] === value) {
-      document.getElementById(box).style.backgroundColor = "green";
+      clearInterval(innerTimer);
+      // displaying arrow
+      arrowIcons[counter].style.display = "block";
+      document.getElementById(box).style.backgroundColor = successColor;
       alert("Element Found At Index " + counter);
       location.reload();
       clearInterval(timer);
@@ -32,12 +44,19 @@ function binarySearch(arr, x, start, end) {
 
   // Find the middle index
   let mid = Math.floor((start + end) / 2);
-  let box = `box${mid}`;
-  document.getElementById(box).style.backgroundColor = "red";
+  let previousMid = mid;
+  let box = `box-wrapper-${mid}`;
+
+  // displaying arrow and color
+  arrowIcons[mid].style.display = "block";
+  const timer = setTimeout(() => {
+    document.getElementById(box).style.backgroundColor = failureColor;
+  }, 500);
 
   // Compare mid with given key x
   if (arr[mid] === x) {
-    document.getElementById(box).style.backgroundColor = "green";
+    document.getElementById(box).style.backgroundColor = successColor;
+    clearInterval(timer);
     alert("Element Found At Index " + mid);
     location.reload();
     return true;
@@ -47,12 +66,16 @@ function binarySearch(arr, x, start, end) {
   if (arr[mid] > x) {
     // search in the left half of mid
     setTimeout(() => {
+      // hiding arrow
+      arrowIcons[previousMid].style.display = "none";
       return binarySearch(arr, x, start, mid - 1);
     }, 1000);
   } else {
     // If element at mid is smaller than x,
     // search in the right half of mid
     setTimeout(() => {
+      // hiding arrow
+      arrowIcons[previousMid].style.display = "none";
       return binarySearch(arr, x, mid + 1, end);
     }, 1000);
   }
